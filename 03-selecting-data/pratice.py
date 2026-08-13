1177612999e7c5347a28ab696bae7f2662abee70
KrishnansuMohapatra <krishnansumohapatra20@gmail.com>
2026-08-13 15:26:01 +0530

    Create practice.py for selecting data with pandas
    Added a practice challenge for data selection using pandas.

---
+++ 03-selecting-data/pratice.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv("sales.csv")
+
+# 🧠 Practice Challenge
+#
+# 1. Select the Product and City columns.
+#
+# 2. Select only rows 2 through 4.
+#
+# 3. Try combining both:
+#    Product + City, rows 2 through 4.
+#
+# Write your solution below:
